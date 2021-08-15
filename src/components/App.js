61091c37b461b65bed4a5e2f6596c6df2486e1d5
Jarad Solomon <jaradsolomon@gmail.com>
2021-08-15 15:29:57 -0500

    C.A.

--- src/components/App.js
+++ src/components/App.js
@@ -17,27 +17,53 @@ function App() {
     })
   }, [])
 
+  function update(){
+const newToys = toys.filter()
+    setToys(newToys)
+  }
+
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
   function handleLike(id, likes){
-    console.log(id, likes)
+    // console.log(id, likes)
     fetch(`http://localhost:3001/toys/${id}`, {
       method: "PATCH",
       headers: {    "Content-type": "application/json"  },
       body: JSON.stringify({ 'likes': likes + 1 })
-    }).then(res => console.log(res.status))
+    }).then(res => res.json())
+    .then(data => {
+      const newL = toys.map(toy => {
+        if(toy.id === id){
+          toy.likes = likes + 1
+          return toy
+        } else {return toy}})
+        setToys(newL)
+      })
+
+ 
+
+    
   }
 
   function handleDel(id){
 fetch(`http://localhost:3001/toys/${id}`, {
 method : 'DELETE',
 })
-.then(res => res.json())
-// .then(r => setToys(toys))
+.then(res => {
+  const newL = toys.filter(toy => toy.id !== id
+  )
+  setToys(newL)
+})
 
-  }
+
+
+}
+
+
+
+  
 
   return (
     <>
